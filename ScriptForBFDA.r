36afabb8a206bd6b899471fc9ea94c1eaf88c25d
tkWong3004 <t.k.wong3004@gmail.com>
2024-09-06 12:04:52 +0200

    Change of text in codes

--- ScriptForBFDA.r
+++ ScriptForBFDA.r
@@ -13,7 +13,7 @@ source("twosample.r")
 #################################################
 # Specification 
 # Mode:
-Mode = 1 # 1: A priori BFDA   0: post hoc BFDA
+Mode = 1 # 1: A priori BFDA   0: BFDA for  a fixed sample size
 
 #for post hoc BFDA
 N1 = 51  # sample size for group 1 or total sample size for one sample and paired t-test
@@ -25,21 +25,21 @@ N2 = 51  # sample size for group 2
 # Analysis prior 
 BF_bound =  3
 Power = .8        # desired probability of compelling evidence for the alternative given a design prior
-Models = "NLP"    # Other prior distributions: "t-distribution"  , "Normal"  , "NLP"
+Models = "NLP"    # Prior distributions: "t-distribution"  , "Normal"  , "NLP" , "Cauchy"
 location = 0      # location parameter
 scale = .45       # scaling parameter or the standard deviation for the normal distribution
 df = 1            # degree of freedom for t-distribution
-hypothesis = hypothesis_d =  ">"  #"!=": two-sided   ">": effect size greater than zero "<": effect size smaller than zero
+hypothesis = ">"  #"!=": two-sided   ">": effect size greater than zero "<": effect size smaller than zero
 r = 1             # Ratio of sample size in group 2 over the one in group 1 for independent t-test
 Design_prior = 1  # 1: Analysis prior and design prior are the same
                   # 0: Analysis prior and design prior are different
 
 # Design priors
-Model_d = "NLP"   # Alternative models: "t-distribution" "Normal" "NLP"
+Model_d = "NLP"   # Prior distributions: "t-distribution"  , "Normal"  , "NLP" , "Cauchy"
 location_d = 0    # location parameter
-scale_d = .3      # scaling parameter or the standard deviation for normal model
+scale_d = .3      # scaling parameter or the standard deviation for normal distribution
 df_d = 1          # degree of freedom for t-distribution
-
+hypothesis_d = hypothesis 
 
 # For independent t-test, the BFDA can be done by:
 print (Table_two (BF_bound,r,Power,
